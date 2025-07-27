9edce604f9b767138558d8cc984db9fd8228d8e8
Qirrat098 <qirratazam098@gmail.com>
2025-07-27 15:11:21 +0500

    File handling added

--- code_1.py
+++ code_1.py
@@ -1,3 +1,18 @@
-import pandas as pd
+HISTORY_FILE = "history.txt"
+
+def show_history():
+    file = open(HISTORY_FILE, "r")
+    lines = file.readlines()
+    if len(lines) == 0:
+        print("No history available.")
+    else:
+        for line in reversed(lines):
+            print(line.strip())
+    file.close()
+
+def clear_history():
+    file = open(HISTORY_FILE, "w")
+    file.write("")  # Clear the file by writing an empty string
+    file.close()
 
 
